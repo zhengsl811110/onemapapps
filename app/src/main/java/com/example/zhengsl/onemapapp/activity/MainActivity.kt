1a777246edfa9f4dd3327385b9367600bd376a27
zhengsl <844304963@qq.com>
2018-07-21 23:16:28 +0800

    添加初始化范围及GPS定位

--- app/src/main/java/com/example/zhengsl/onemapapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/zhengsl/onemapapp/activity/MainActivity.kt
@@ -11,6 +11,7 @@ import com.esri.arcgisruntime.geometry.Point
 import com.esri.arcgisruntime.layers.ArcGISTiledLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.LocationDisplay
 import com.example.zhengsl.onemapapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -18,15 +19,12 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    var map:ArcGISMap?=null
+    var locationDisplay:LocationDisplay?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -34,7 +32,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
-
+        resetExtent.setOnClickListener {
+            setViewPointCenter()
+        }
+        gpsLocation.setOnClickListener {
+            locationDisplay?.autoPanMode = LocationDisplay.AutoPanMode.RECENTER
+            locationDisplay?.startAsync()
+        }
         init()
     }
 
@@ -61,11 +65,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun init() {
         var layer = ArcGISTiledLayer("http://58.210.9.131/arcgis/rest/services/CS/GXQMAP/MapServer")
         val baseMap = Basemap(layer)
-        val map = ArcGISMap(baseMap)
+        map = ArcGISMap(baseMap)
         mapView.map = map
-//        val center=Point()
-//        mapView.setViewpointCenterAsync()
+        map?.addDoneLoadingListener {
+            setViewPointCenter()
+        }
+        //GPS
+        locationDisplay = mapView.locationDisplay;
+        locationDisplay?.isShowLocation = true;
+    }
 
+    private fun setViewPointCenter() {
+        val center = Point(36059.0, 46659.0, map?.spatialReference)
+        var scale = 36111.909643
+        mapView.setViewpointCenterAsync(center, scale)
     }
 
 
